5d12796add650b359928fdceb7a29474c0b0b72d
Felipe-Sudario <felipe.campos79@etec.sp.gov.br>
2025-02-13 14:30:48 -0300

    paginação feita - 13/02/25

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "topo.php";
+include "topo.php";
 ?>
  
 <?php
--- topo.php
+++ topo.php
@@ -27,9 +27,3 @@
     <li><a href="collapsible.html">Javascript</a></li>
     <li><a href="mobile.html">Mobile</a></li>
   </ul>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  <script type="text/javascript">
-        M.AutoInit();
-    </script>
-</body>
-</html>
