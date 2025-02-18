04b2cbae79d95288d1d19faf09ee448420b072ef
gsbaldass <guibaldasso1@gmail.com>
2025-02-18 17:38:03 -0300

    navbar feita

--- meio.php
+++ meio.php
@@ -1,7 +1,3 @@
-<section>dsada</section>
-datefmt_set_calendar
-<section>dsadsadasd
-
-shm_detachasd
-sa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam laborum nisi eligendi sapiente quaerat molestiae quo totam rerum quisquam, voluptatum quibusdam pariatur facilis maxime tempore sint. Deserunt dolore quod sit!
-</section>
+<div class="meio">
+    <img src="img/banner.svg" id="banner">
+</div>
