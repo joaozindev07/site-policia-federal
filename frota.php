970cc4606f072ef839a8299edac4815986d1d72a
AndrX <164493523+AndreLuizDeSouzaMariano@users.noreply.github.com>
2025-02-19 19:27:31 -0300

    boa noite

---
+++ frota.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Veículo da Polícia</title>
+    <link rel="stylesheet" href="css/frota.css">
+</head>
+<body>
+
+    <div class="novo-banner">
+        <img src="img/banner2.svg" alt="Banner">
+    </div>
+
+    <section class="secao-carro-novo">
+        <div class="imagem-carro-novo">
+            <img src="img/image 22.png" alt="Mitsubishi Pajero Full">
+        </div>
+        <div class="texto-descricao-novo">
+            <h2>Mitsubishi Pajero Full</h2>
+            <p>O Mitsubishi Pajero Full é ganha notoriedade cada vez que alguém importante é levado para as dependências da Polícia Federal. Foi em um deles que o ex-presidente Luiz Inácio Lula da Silva foi transportado da sede do Sindicato dos Metalúrgicos de São Bernardo do Campo, no dia 7/4, até a PF, na Lapa, em São Paulo.</p>
+        </div>
+    </section>
+
+    <section class="secao-carro-novo">
+        <div class="imagem-carro-novo">
+            <img src="img/image 23.png" alt="Mitsubishi Pajero HPE">
+        </div>
+        <div class="texto-descricao-novo">
+            <h2>Mitsubishi Pajero HPE</h2>
+            <p>Outro modelo da Mitsubishi que também é bastante utilizado pela Polícia Federal é o Pajero HPE. Tem capacidade para levar com relativo conforto sete pessoas.</p>
+        </div>
+    </section>
+
+    <section class="secao-carro-novo">
+        <div class="imagem-carro-novo">
+            <img src="img/image 24.png" alt="Ford Focus">
+        </div>
+        <div class="texto-descricao-novo">
+            <h2>Ford Focus</h2>
+            <p>Além dos modelos da Mitsubishi, o órgão utiliza também o Ford Focus, sedã da marca de origem norte-americana, produzido na Argentina.</p>
+        </div>
+    </section>
+
+    <section class="secao-carro-novo">
+        <div class="imagem-carro-novo">
+            <img src="img/image 25.png" alt="Chevrolet Blazer">
+        </div>
+        <div class="texto-descricao-novo">
+            <h2>Chevrolet Blazer</h2>
+            <p>A Chevrolet também é uma fornecedora frequente dos órgãos policiais, desde os tempos da Veraneio 'Vascaína', utilizada nos tempos de repressão da ditadura militar. Mais recentemente, a Blazer também prestou serviços à Polícia Federal.</p>
+        </div>
+    </section>
+
+    <section class="secao-carro-novo">
+        <div class="imagem-carro-novo">
+            <img src="img/image 26.png" alt="Toyota Corolla">
+        </div>
+        <div class="texto-descricao-novo">
+            <h2>Toyota Corolla</h2>
+            <p>O sedã Toyota Corolla é outro modelo que presta serviços à PF, conhecido por sua confiabilidade, conforto e eficiência.</p>
+        </div>
+    </section>
+
+    <section class="secao-carro-novo">
+        <div class="imagem-carro-novo">
+            <img src="img/image 27.png" alt="Nissan XTerra">
+        </div>
+        <div class="texto-descricao-novo">
+            <h2>Nissan XTerra</h2>
+            <p>O SUV XTerra era um modelo derivado da picape Frontier de geração anterior, e também foi utilizado pela PF em operações urbanas e rurais.</p>
+        </div>
+    </section>
+
+    <section class="secao-carro-novo">
+        <div class="imagem-carro-novo">
+            <img src="img/image 28.png" alt="Nissan Frontier">
+        </div>
+        <div class="texto-descricao-novo">
+            <h2>Nissan Frontier</h2>
+            <p>A Nissan forneceu no passado tanto a picape Nissan como o SUV XTerra à PF. Assim como os modelos da Mitsubishi, os da Nissan são utilizados também em operações rurais, em áreas nas quais a tração 4x4 é importante.</p>
+        </div>
+    </section>
+
+</body>
+</html>
--- rodape.php
+++ rodape.php
@@ -1,21 +1,15 @@
 <div class="footer">
-  <div class="sessao-footer2">  <!--expecifico para a logo do governo -->
-    <img src="img/logo_governo.png">
+  <div class="sessao-footer2">
+    <img src="img/logo_governo.png" alt="Logo do Governo">
   </div>
-  <img src="img/logo_pf.png" alt="">
+  
+  <img src="img/logo_pf.png" alt="Logo da Polícia Federal">
+  
   <div class="sessao-footer">
-    <a href=""><img src="img/youtube_icon.png" alt=""></a>
-    <a href=""><img src="img/twitter_icon.png" alt=""></a>
-    <a href=""><img src="img/linkedin_icon.png" alt=""></a>
-    <a href=""><img src="img/instagram_icon.png" alt=""></a>
-    <a href=""><img src="img/facebook_icon.png" alt=""></a>
+    <a href=""><img src="img/youtube_icon.png" alt="YouTube"></a>
+    <a href=""><img src="img/twitter_icon.png" alt="Twitter"></a>
+    <a href=""><img src="img/linkedin_icon.png" alt="LinkedIn"></a>
+    <a href=""><img src="img/instagram_icon.png" alt="Instagram"></a>
+    <a href=""><img src="img/facebook_icon.png" alt="Facebook"></a>
   </div>
 </div>
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  <script type="text/javascript">
-        M.AutoInit();
-    </script>
-</body>
-</html>
