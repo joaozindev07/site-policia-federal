9cbe9b47383cd966fbcca456eee8eee6a42a9319
joaozindev07 <joao.soares176@etec.sp.gov.br>
2025-02-19 08:34:54 -0300

    add screen contact

--- index.php
+++ index.php
@@ -1,29 +1,33 @@
 <?php
-include "topo.php";
+ include_once "topo.php";
 ?>
- 
+
 <?php
- 
 if (isset($_GET['p'])){
-    $p = $_GET['p'];
-    switch ($p){
-        case 1: require_once "meio.php";
-        break;
-        case 2: require_once "quemsomos.php";
-        break;
-        case 3: require_once "catalogo.php";
-        break;
-        case 4: require_once "solicitacao.php";
-        break;
-        case 5: require_once "contato.php";
-        break;
-    }
+$p = $_GET['p'];
+switch ($p){
+case 1: require_once "meio.php";
+break;
+case 2: require_once "origem.php";
+break;
+case 3: require_once "timeline.php";
+break;
+case 4: require_once "lideres.php";
+break;
+case 5: require_once "frota.php";
+break;
+case 6: require_once "ingressar.php";
+break;
+case 7: require_once "contato.php";
+break;
 }
+}
+
 else {
-    require_once "meio.php";
+require_once "meio.php";
 }
 ?>
- 
+
 <?php
 require_once "rodape.php";
 ?>
