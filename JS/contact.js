3eed58b2c86e31a03725975756f9dfc4c7f9344a
AndrX <164493523+AndreLuizDeSouzaMariano@users.noreply.github.com>
2025-02-19 21:03:46 -0300

    opa

--- JS/contact.js
+++ JS/contact.js
@@ -1,15 +1,14 @@
 function validateForm() {
   let valid = true;
-  clearErrors(); // Limpar erros anteriores
+  clearErrors(); 
 
-  // Validação do nome
   let nome = document.getElementById("nome").value;
   if (nome.trim() === "") {
     showError("nome", "Nome é obrigatório");
     valid = false;
   }
 
-  // Validação do e-mail
+
   let email = document.getElementById("email").value;
   let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   if (!emailPattern.test(email)) {
@@ -62,3 +61,4 @@ function clearErrors() {
     document.getElementById(field + "-error").textContent = "";
   });
 }
+
--- JS/script.js
+++ JS/script.js
@@ -5,17 +5,16 @@ document.querySelectorAll('.imagem-carro-novo').forEach(item => {
         const width = item.offsetWidth;
         const height = item.offsetHeight;
 
-        const rotateX = (y / height - 0.5) * 10;  // Pequena rotação no eixo X
-        const rotateY = (x / width - 0.5) * -10;  // Pequena rotação no eixo Y
+        
+        const depthX = (x / width - 0.5) * 10; 
+        const depthY = (y / height - 0.5) * 2; 
 
-        // Aumenta a escala e aplica rotação bem sutil
-        item.querySelector('img').style.transform = `scale(1.05) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-        item.querySelector('img').style.boxShadow = `0 10px 20px rgba(0, 0, 0, 0.2)`; // Sombra suave
+        item.querySelector('img').style.transform = `scale(1.1) translateZ(30px) translateX(${depthX}px) translateY(${depthY}px)`;
+        item.querySelector('img').style.boxShadow = `0 10px 20px rgba(0, 0, 0, 0.2)`; 
     });
 
     item.addEventListener('mouseleave', () => {
-        // Volta à escala normal sem rotação
-        item.querySelector('img').style.transform = 'scale(1) rotateX(0) rotateY(0)';
-        item.querySelector('img').style.boxShadow = 'none'; // Remove sombra
+        item.querySelector('img').style.transform = 'scale(1) translateZ(0) translateX(0) translateY(0)';
+        item.querySelector('img').style.boxShadow = 'none'; 
     });
 });
