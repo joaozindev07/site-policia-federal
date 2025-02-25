a21a219f27443f5cd76edcdf76528443ee3d1ef0
Aluno <aluno@devisate.local>
2025-02-25 11:35:04 -0300

    recomeco da home

--- meio.php
+++ meio.php
@@ -1,35 +1,50 @@
 <!-- imagem banner -->
 <img id="banner" src="img/banner3.svg" alt="">
 
-<div class="historia" id="origem">
-    <h1 class="league-gothic-font" >História da Polícia Federal</h1>
-    <div class="texto-historia">
+<div class="grid-container">
+        <div class="item item1">        <h5>Origens e Primeiros Anos</h5>
+        <p>
+            Existem diferentes opiniões sobre a origem da Polícia Federal. A vertente histórica é defendida por aqueles que consideram suas origens remotas na criação da Intendência Geral de Polícia, em 1808, por D. João VI, no surgimento do Código de Processo Criminal de 1832 e na instituição do cargo de Chefe de Polícia, criado com a reforma do código em 1841, pela Lei nº 261, sendo Eusébio de Queiróz o primeiro a ocupá-lo. Essa nomenclatura permanece até hoje em algumas Polícias Civis, como a do Rio de Janeiro.
+        </p>
+        <p>
+            Outros estudiosos atribuem a origem da Polícia Federal ao período do Estado Novo (1937-1945), durante o governo de Getúlio Vargas. Em 28 de março de 1944, por meio do Decreto-Lei nº 6.378, a antiga Polícia Civil do Distrito Federal foi renomeada para Departamento Federal de Segurança Pública (DFSP). A mudança visava permitir que a polícia do Rio de Janeiro atuasse em outros estados, embora sua estrutura institucional ainda remontasse ao início do século XIX.
+        </p></div>
+        <div class="item item2">Item 2 (menor)</div>
+        <div class="item item3"><ul class="collapsible">
+  <li>
+    <div class="collapsible-header">
+      <i class="material-icons">filter_drama</i>
+      First
+      <span class="new badge">4</span></div>
+    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+  </li>
+  <li>
+    <div class="collapsible-header">
+      <i class="material-icons">place</i>
+      Second
+      <span class="badge">1</span></div>
+    <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+  </li>
+</ul>
+</div>
+</div>
+
+
+<div class="parent">
+    <div class="div1">
         <h5>Origens e Primeiros Anos</h5>
         <p>
             Existem diferentes opiniões sobre a origem da Polícia Federal. A vertente histórica é defendida por aqueles que consideram suas origens remotas na criação da Intendência Geral de Polícia, em 1808, por D. João VI, no surgimento do Código de Processo Criminal de 1832 e na instituição do cargo de Chefe de Polícia, criado com a reforma do código em 1841, pela Lei nº 261, sendo Eusébio de Queiróz o primeiro a ocupá-lo. Essa nomenclatura permanece até hoje em algumas Polícias Civis, como a do Rio de Janeiro.
         </p>
-        <section>
-            <aside>
-                <p>
-                    Outros estudiosos atribuem a origem da Polícia Federal ao período do Estado Novo (1937-1945), durante o governo de Getúlio Vargas. Em 28 de março de 1944, por meio do Decreto-Lei nº 6.378, a antiga Polícia Civil do Distrito Federal foi renomeada para Departamento Federal de Segurança Pública (DFSP). A mudança visava permitir que a polícia do Rio de Janeiro atuasse em outros estados, embora sua estrutura institucional ainda remontasse ao início do século XIX.
-                </p>
-                <p>
-                    O DFSP cresceu em tamanho, importância e atribuições até que, em 21 de abril de 1960, com a transferência da capital federal do Rio de Janeiro para Brasília, muitos integrantes do departamento optaram por permanecer no Rio e integrar a Polícia Civil do Estado da Guanabara, por meio do Departamento Estadual de Segurança Pública, que herdou uma tradição institucional de mais de 150 anos. Isso resultou em uma carência de pessoal, materiais e arquivos essenciais para a corporação de Brasília, já que setores inteiros permaneceram no Rio de Janeiro — como ocorreu com a Divisão de Polícia Política e Social, que passou a integrar a administração estadual, mas continuou atuando nacionalmente.
-                </p>
-            </aside>
-            <img src="img/dfsp_logo.png" alt="">
-        </section>
-        <hr>
         <p>
-            Em 1960, ocorreu a fusão do DFSP com a Guarda Especial de Brasília (GEB), responsável por manter a ordem durante a construção da nova capital, mantendo-se a denominação DFSP.
-        </p>
-        <p>
-            Buscando modernizar sua estrutura, o DFSP passou a se inspirar em modelos policiais de países desenvolvidos, como Estados Unidos, Canadá e Inglaterra. No final de 1960, o Poder Executivo encaminhou ao Congresso um anteprojeto de lei para a criação de um organismo policial de âmbito nacional. A proposta foi concretizada com a aprovação da Lei nº 4.483, de 16 de novembro do mesmo ano, reorganizando o DFSP e ampliando sua atuação federal.
-        </p>
-        <p>
-            Por fim, a Constituição de 1967, em seu artigo 210, alterou a denominação do órgão para Departamento de Polícia Federal, definindo suas funções no artigo 8º, inciso VII, alíneas "a" a "d". A mudança foi efetivada em fevereiro do mesmo ano.
+            Outros estudiosos atribuem a origem da Polícia Federal ao período do Estado Novo (1937-1945), durante o governo de Getúlio Vargas. Em 28 de março de 1944, por meio do Decreto-Lei nº 6.378, a antiga Polícia Civil do Distrito Federal foi renomeada para Departamento Federal de Segurança Pública (DFSP). A mudança visava permitir que a polícia do Rio de Janeiro atuasse em outros estados, embora sua estrutura institucional ainda remontasse ao início do século XIX.
         </p>
     </div>
+    <div class="div2">2</div>
+    <div class="div3">     
+        O DFSP cresceu em tamanho, importância e atribuições até que, em 21 de abril de 1960, com a transferência da capital federal do Rio de Janeiro para Brasília, muitos integrantes do departamento optaram por permanecer no Rio e integrar a Polícia Civil do Estado da Guanabara, por meio do Departamento Estadual de Segurança Pública, que herdou uma tradição institucional de mais de 150 anos. Isso resultou em uma carência de pessoal, materiais e arquivos essenciais para a corporação de Brasília, já que setores inteiros permaneceram no Rio de Janeiro — como ocorreu com a Divisão de Polícia Política e Social, que passou a integrar a administração estadual, mas continuou atuando nacionalmente.</div>
+    </div>
+    
     <div class="atuacao-historia">
         <section>
             <h5>Polícia Federal atualmente</h5>
@@ -59,72 +74,72 @@
         </section>
     </div>
 </div>
-<!-- linha do tempo -->
-
-<div class="timeline" id="linhadotempo">
-    <h1 class="league-gothic-font" >Linha do Tempo</h1>
-    <div class="circulo"></div>
-    <div class="linha"></div>
-    <div class="textbox-timeline">
-        <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">1808</h2>
+    <!-- linha do tempo -->
+    
+    <div class="timeline" id="linhadotempo">
+        <h1 class="league-gothic-font" >Linha do Tempo</h1>
+        <div class="circulo"></div>
+        <div class="linha"></div>
+        <div class="textbox-timeline">
+            <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">1808</h2>
             <p>
-            Criação da Intendência Geral de Polícia da Corte e do Estado do Brasil por Dom João VI, um dos primeiros órgãos policiais do país..
-    </div>
+                Criação da Intendência Geral de Polícia da Corte e do Estado do Brasil por Dom João VI, um dos primeiros órgãos policiais do país..
+            </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">1891</h2>
-            <p>
-                A Constituição Republicana prevê a necessidade de uma polícia federal para atuar em crimes contra a União.
-            </p>
+        <p>
+            A Constituição Republicana prevê a necessidade de uma polícia federal para atuar em crimes contra a União.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">1960</h2>
-            <p>
-                A PF assume a emissão de passaportes e começa a atuar na segurança de fronteiras e aeroportos.
-            </p>
+        <p>
+            A PF assume a emissão de passaportes e começa a atuar na segurança de fronteiras e aeroportos.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">1967</h2>
-            <p>
-                Durante o regime militar, o DFSP é reformulado e passa a se chamar <b>Polícia Federal</b> (PF), com novas atribuições e estrutura.
-            </p>
+        <p>
+            Durante o regime militar, o DFSP é reformulado e passa a se chamar <b>Polícia Federal</b> (PF), com novas atribuições e estrutura.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">1988</h2>
-            <p>
-                A Constituição Federal define a PF como responsável pela investigação de crimes federais, incluindo corrupção, tráfico internacional e segurança nacional.
-            </p>
+        <p>
+            A Constituição Federal define a PF como responsável pela investigação de crimes federais, incluindo corrupção, tráfico internacional e segurança nacional.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">1995</h2>
-            <p>
-                Criação do <b>Plano Nacional de Segurança Pública</b>, fortalecendo a PF no combate ao crime organizado e à lavagem de dinheiro.
-            </p>
+        <p>
+            Criação do <b>Plano Nacional de Segurança Pública</b>, fortalecendo a PF no combate ao crime organizado e à lavagem de dinheiro.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">2004</h2>
-            <p>
-                A PF inicia operações de grande repercussão contra a corrupção, como a <b>Operação Vampiro</b>, que investigou fraudes no Ministério da Saúde.
-            </p>
+        <p>
+            A PF inicia operações de grande repercussão contra a corrupção, como a <b>Operação Vampiro</b>, que investigou fraudes no Ministério da Saúde.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">2014</h2>
-            <p>
-                Início da Operação <b>Lava Jato</b>, maior investigação de corrupção da história do Brasil, envolvendo políticos e grandes empresas.
-            </p>
+        <p>
+            Início da Operação <b>Lava Jato</b>, maior investigação de corrupção da história do Brasil, envolvendo políticos e grandes empresas.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
         <h2 class="league-gothic-font"><img src="img/mapas-e-bandeiras.png" alt="">2020</h2>
-            <p>
-                A PF assume papel fundamental no combate ao tráfico de drogas e crimes ambientais, como no desmatamento ilegal da Amazônia.
-            </p>
+        <p>
+            A PF assume papel fundamental no combate ao tráfico de drogas e crimes ambientais, como no desmatamento ilegal da Amazônia.
+        </p>
     </div>
     <div class="linha"></div>
     <div class="textbox-timeline">
